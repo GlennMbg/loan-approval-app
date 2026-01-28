93c145b91fd3628406fbfb1842b604bda185f70d
Glenn Mboga <mbogaglenn@gmail.com>
2026-01-28 14:19:18 +0100

    test login

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 
+
+password = st.text_input("Enter the password to access the app:", type="password")
+if password == st.secrets["app_password"]:
+    st.success("Access granted!")
 # config de la page
-st.set_page_config(
-    page_title="Loan aproval Prediction",
-    page_icon="🚀",
-    layout="wide"
-)
+    st.set_page_config(
+        page_title="Loan aproval Prediction",
+        page_icon="🚀",
+        layout="wide"
+    )
 
-# titre 
-st.title("Loan Approval Prediction App 🚀")
-st.markdown("## About this App")
+    # titre 
+    st.title("Loan Approval Prediction App 🚀")
+    st.markdown("## About this App")
 
-# contenu
-st.write("Bienvenue dans l'application de prédiction d'approbation de prêt. ")
+    # contenu
+    st.write("Bienvenue dans l'application de prédiction d'approbation de prêt. ")
+
+else:
+    st.error("Access denied")
+    st.stop()
